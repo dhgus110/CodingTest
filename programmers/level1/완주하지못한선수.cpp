bdc29516b37a22f1e8becafc0c6215b7ff8b1a82
joh <dhgus110@naver.com>
2021-06-24 22:09:23 +0900

    완주하지못한선수

---
+++ programmers/level1/완주하지못한선수.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion)
+{
+    string answer = "";
+    map<string, int> map;
+    for (auto c : completion)
+    {
+        map[c] += 1;
+    }
+    for (auto p : participant)
+    {
+        map[p] -= 1;
+        if (map[p] < 0)
+            answer = p;
+    }
+
+    return answer;
+}
+
+int main()
+{
+    vector<string> s1 = {"q", "q", "w", "r", "e", "e", "w", "s", "b", "a", "1", "5"};
+    vector<string> s2 = {"q", "q", "w", "r", "e", "5", "1", "e", "w", "s", "b"};
+    string s = solution(s1, s2);
+    cout << endl;
+    cout << s;
+}
