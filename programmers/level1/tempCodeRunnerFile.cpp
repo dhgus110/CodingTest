4234db4156c7ccef37815c4ff0ea73c45218ff65
joh <dhgus110@naver.com>
2021-07-09 16:49:47 +0900

    codingTest

--- programmers/level1/tempCodeRunnerFile.cpp
+++ programmers/level1/tempCodeRunnerFile.cpp
@@ -1,1 +1,7 @@
-3 3 2 
+answer = stage1(new_id);
+    answer = stage2(answer);
+    answer = stage3(answer);
+    answer = stage4(answer);
+    answer = stage5(answer);
+    answer = stage6(answer);
+    answer = stage7(answer);
