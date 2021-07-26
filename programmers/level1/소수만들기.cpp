afe0e6ae7a239f7191cec7d9e386ad6d7084c1f4
joh <dhgus110@naver.com>
2021-07-27 01:01:59 +0900

    소수 만들기

---
+++ programmers/level1/소수만들기.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+bool isPrime(int num)
+{
+    if (num == 0 || num == 1)
+        return false;
+    for (int i = 2; i < num / 2; i++)
+        if (num % i == 0)
+            return false;
+    return true;
+}
+
+int solution(vector<int> nums)
+{
+    int answer = 0;
+    for (int z = 0; z < nums.size(); z++)
+    {
+        for (int x = z + 1; x < nums.size() ; x++)
+        {
+            for (int c = x + 1; c < nums.size() ; c++)
+            {
+                int sum = nums[z] + nums[x] + nums[c];
+                if (isPrime(sum))
+                    answer++;
+            }
+        }
+    }
+    return answer;
+}
