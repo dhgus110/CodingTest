8c1d696de857a8afe38dede68484d4014cd6e6ef
joh <dhgus110@naver.com>
2021-12-31 16:27:41 +0900

    큐

---
+++ programmers/level2/더맵게.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+using namespace std;
+
+int solution(vector<int> scoville, int k)
+{
+    int answer = 0;
+    priority_queue<int,vector<int>,greater<int>> pq(scoville.begin(), scoville.end());
+    if (pq.top() >= k)
+        return -1;
+
+    while (pq.top() < k)
+    {
+        if(pq.size()<2) return -1;
+        int firstMild = pq.top(); pq.pop();
+        int secondMild = pq.top(); pq.pop();
+        if (secondMild == 0) return -1;
+        
+        int mix = firstMild + secondMild * 2;
+        //cout<<"first : "<<firstMild<<"..second :"<<secondMild<<".. Mix : "<<mix<<endl;
+        pq.push(mix);
+        answer++;
+        // cout<<"top? "<<pq.top()<<endl;
+    }
+    cout<<answer;
+    return answer;
+}
