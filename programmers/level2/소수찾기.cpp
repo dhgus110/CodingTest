93a1243275df4f1c090c095b1cbf216defee5549
joh <dhgus110@naver.com>
2022-12-06 23:54:15 +0900

    완전탐색

---
+++ programmers/level2/소수찾기.cpp
@@ -0,0 +1,43 @@
+#include <string>
+#include <map>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+map<int, bool> M;
+
+bool isPrime(string num){
+    int n = stoi(num);
+    
+    if(M[n]) return false; // 이미 확인한 수라면 중복이기 때문에 false 리턴
+    M[n] = true;
+    if(n == 1 || n == 0) return false;
+    if(n == 2) return true;
+    
+    for(int i=2; i<=sqrt(n); i++){
+        if(n % i == 0){ 
+            M[n] == -1;
+            return false;
+        }              
+    }
+    M[n] == 1;
+    return true;
+}
+
+int solution(string numbers) {
+    int answer = 0;
+    // 오름차순 정렬
+    sort(numbers.begin(), numbers.end());
+    // 나올 수 있는 모든 조합에 대해서
+    // 한 자리수인 경우, 두 자리수인 경우, ..., numbers.size() 자리수인 경우
+    // 를 모두 검사
+    do{
+        for(int i=1; i<=numbers.size(); i++){
+            string num = numbers.substr(0, i);         
+            if(isPrime(num)) answer++;
+        }
+    }while(next_permutation(numbers.begin(), numbers.end()));
+    
+    return answer;
+}
