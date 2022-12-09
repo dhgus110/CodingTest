4922bee1268693a48d03b474f1cac5652b9c501e
joh <dhgus110@naver.com>
2022-12-09 23:47:45 +0900

    탐욕법

---
+++ programmers/level2/구명보트.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 0;
+
+    sort(people.begin(),people.end());
+
+    int right = people.size()-1;
+    int left = 0;
+    while(left<=right){
+        answer+= 1;
+        if(people[right]+people[left] <= limit){
+            left++;
+            right--;
+        }
+        else{
+            right--;
+        }
+
+    }
+
+    return answer;
+}
