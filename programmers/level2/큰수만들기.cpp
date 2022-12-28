e4065ee00fd2ba00d4e3b1b09365207ba4690123
dhgus110 <45159737+dhgus110@users.noreply.github.com>
2022-12-28 23:51:09 +0900

    Add files via upload

---
+++ programmers/level2/큰수만들기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string number, int k) {
+    for (int i = 0; i < number.size() && k != 0; i++) {
+        if (number[i] < number[i + 1]) {
+            number.erase(i, 1);
+            if (i == 0)      i -= 1;
+            else            i -= 2;
+            k--;
+        }
+    }
+    if (k > 0)
+        number.erase(number.size() - k, k);
+    return number;
+}
