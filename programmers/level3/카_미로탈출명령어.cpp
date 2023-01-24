6e9c74adeb7045befd497925f48ddef04f975b23
dhgus110 <45159737+dhgus110@users.noreply.github.com>
2023-01-24 22:32:47 +0900

    Update 카_미로탈출명령어.cpp
    while()문으로 k번만큼만 움직여서 풀기 실패 (테스트 18번만 틀림) -> 그냥 dfs로 풀었음

--- programmers/level3/카_미로탈출명령어.cpp
+++ programmers/level3/카_미로탈출명령어.cpp
@@ -5,27 +5,50 @@
 using namespace std;
 
 int _n, _m, _r, _c, _k;
+int dx[4] = { 1,0,0,-1 };
+int dy[4] = { 0,-1,1,0 };
+char d[4] = { 'd','l','r','u' };
+string answer = "";
+bool check = false;
 
 bool LimitArea(int cx, int cy) {
     if (cx > _n || cy > _m || cx < 1 || cy < 1)return false;
     return true;
 }
 
-bool CheckDistance(int cx, int cy) {
+bool CheckDistance(int cx, int cy, int ck) {
     int distance = abs(cx - _r) + abs(cy - _c);
-    if (distance < _k)return true;
+    if (distance > ck)return true;
     return false;
 }
 
+void dfs(int curx, int cury, int curk, string path) {
+    if (curk == 0 && curx == _r && cury == _c) {
+        check = true;
+        answer = path;
+        return;
+    }
+
+    for (int i = 0; i < 4; i++) {
+        int gox = curx + dx[i];
+        int goy = cury + dy[i];
+        if (!LimitArea(gox, goy)) continue;
+        if (CheckDistance(gox, goy, curk - 1)) continue;
+        dfs(gox, goy, curk - 1, path + d[i]);
+        if (check)return;
+    }
+}
+
 string solution(int n, int m, int x, int y, int r, int c, int k) {
-    string answer = "";
+
     _n = n; _m = m; _k = k; _r = r; _c = c;
 
     int distance = abs(x - r) + abs(y - c);
 
     if ((distance > k) || ((distance % 2) != (k % 2))) return "impossible";
+    dfs(x, y, k, "");
 
-    while (CheckDistance(x, y)) {
+    /*while (CheckDistance(x, y)) {
         if (LimitArea(x + 1, y)) {
             x += 1;
             answer += "d";
@@ -64,7 +87,7 @@ string solution(int n, int m, int x, int y, int r, int c, int k) {
     if (xr < 0)  for (int i = 0; i < abs(xr); i++) answer += "d";
     if (yc > 0)  for (int i = 0; i < yc; i++) answer += "l";
     if (yc < 0)  for (int i = 0; i < abs(yc); i++) answer += "r";
-    if (xr > 0)  for (int i = 0; i < xr; i++) answer += "u";
+    if (xr > 0)  for (int i = 0; i < xr; i++) answer += "u";*/
 
     return answer;
 }
