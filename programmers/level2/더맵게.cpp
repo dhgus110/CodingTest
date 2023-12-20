1da0aff60a56331c3d03779efb6192324fbe3aa9
joh <dhgus110@naver.com>
2023-12-20 16:48:24 +0900

    힙, 우선순위 큐

--- programmers/level2/더맵게.cpp
+++ programmers/level2/더맵게.cpp
@@ -4,26 +4,46 @@
 #include <iostream>
 using namespace std;
 
-int solution(vector<int> scoville, int k)
-{
+int solution(vector<int> scoville, int k){
     int answer = 0;
-    priority_queue<int,vector<int>,greater<int>> pq(scoville.begin(), scoville.end());
-    if (pq.top() >= k)
-        return -1;
+    priority_queue<int,vector<int>, greater<int>> pq(scoville.begin(), scoville.end());
 
-    while (pq.top() < k)
-    {
-        if(pq.size()<2) return -1;
-        int firstMild = pq.top(); pq.pop();
-        int secondMild = pq.top(); pq.pop();
-        if (secondMild == 0) return -1;
-        
-        int mix = firstMild + secondMild * 2;
-        //cout<<"first : "<<firstMild<<"..second :"<<secondMild<<".. Mix : "<<mix<<endl;
-        pq.push(mix);
+    while(!pq.empty()&&pq.top()<k){
+        int firstMin = pq.top();
+        pq.pop();
+        if(pq.empty()) return -1;
+        int secondMin = pq.top();
+        pq.pop();
+        int newScoville = firstMin + (secondMin * 2);
+        pq.push(newScoville);
         answer++;
-        // cout<<"top? "<<pq.top()<<endl;
     }
-    cout<<answer;
+
     return answer;
+    
 }
+
+
+// int solution(vector<int> scoville, int k)
+// {
+//     int answer = 0;
+//     priority_queue<int,vector<int>,greater<int>> pq(scoville.begin(), scoville.end());
+//     if (pq.top() >= k)
+//         return -1;
+
+//     while (pq.top() < k)
+//     {
+//         if(pq.size()<2) return -1;
+//         int firstMild = pq.top(); pq.pop();
+//         int secondMild = pq.top(); pq.pop();
+//         if (secondMild == 0) return -1;
+        
+//         int mix = firstMild + secondMild * 2;
+//         //cout<<"first : "<<firstMild<<"..second :"<<secondMild<<".. Mix : "<<mix<<endl;
+//         pq.push(mix);
+//         answer++;
+//         // cout<<"top? "<<pq.top()<<endl;
+//     }
+//     cout<<answer;
+//     return answer;
+// }
