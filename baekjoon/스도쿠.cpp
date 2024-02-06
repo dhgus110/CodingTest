79c94fa1c5813a470339f72c629960cfeb93db52
joh <dhgus110@naver.com>
2024-02-06 23:22:40 +0900

    fix

--- baekjoon/스도쿠.cpp
+++ baekjoon/스도쿠.cpp
@@ -14,14 +14,14 @@ string map[101][6][6];
 //후보값 만들기
 void makeCandidate()
 {
-    for (int order = 0; order < gameCount; order++)
+    for (int order = 1; order <= gameCount; order++)
     {
         for (int row = 0; row < 6; row++)
         {
             for (int col = 0; col < 6; col++)
             {
                 if(visited[order][row][col]) continue;
-                
+
                 int check[10]={0,};
                 //x,y axis check
                 for(int i = 0 ; i <6;i++){
@@ -37,6 +37,7 @@ void makeCandidate()
                             check[map[order][i][col][2] -'0']++;
                     }
                 }
+
                 //3x2 sector check 
                 int minY = (row & 1) == 1 ? row - 1 : row;
                 int maxY = minY + 1;
@@ -53,8 +54,7 @@ void makeCandidate()
                     }
                 }
 
-                for(int i = 1 ; i< 10; i++)
-                {
+                for(int i = 1 ; i< 10; i++){
                     if (check[i] == 0) candidate[order][row][col] += to_string(i);
                 }
             }
@@ -63,7 +63,7 @@ void makeCandidate()
 }
 
 void makeSudoku(){
-    for(int order = 0 ; order < gameCount; order++){
+    for(int order = 1 ; order <= gameCount; order++){
         queue<pair<int,int>>  pos;
         for(int a = 0 ;a<6;a++){
             for(int b = 0 ;b<6;b++){
@@ -72,15 +72,15 @@ void makeSudoku(){
                 }
             }
         }
-        // int bb = 0;
+        int bb=0;
         while(!pos.empty()){
-            // bb++;
-            // if(bb>1500)break;
+            bb++;
+            if(bb>62)break;
             int row =pos.front().first;
             int col = pos.front().second;
             pos.pop();
             int blankCount = count(map[order][row][col].begin(),map[order][row][col].end(),'-');
-            int pivot = -1; // ex)  -/5 일 때  -자리에 5보다 작은 수가 들어가야함 pivot =5 ,updown=false
+            int pivot = 0; // ex)  -/5 일 때  -자리에 5보다 작은 수가 들어가야함 pivot =5 ,updown=false
             bool updown =false; // down-false, up-true
             if(blankCount==1 && map[order][row][col].size()>1){
                 if(map[order][row][col][0]=='-'){
@@ -96,14 +96,14 @@ void makeSudoku(){
             //3. 값이 정해지면 map에 저장
             //4. 정해지지 않았으면 cur을 다시 queue에 push
 
-            //후보값은 +1
+            //후보값 살아남으면 양수
             int val[10]={0,}; 
             for(auto cand :candidate[order][row][col]){
                 val[cand-'0']++;
             }
 
             // 1.
-            if(pivot != -1){
+            if(pivot != 0){
                 if(updown)
                     for(int p = 0; p <= pivot; p++) val[p]--;
                 else 
@@ -130,7 +130,7 @@ void makeSudoku(){
             //3x2 sector check 
             int minY = (row & 1) == 1 ? row - 1 : row;
             int maxY = minY + 1;
-            int minX = col / 3 == 0 ? 0 : 3;
+            int minX = col == 0 ? 0 : (col / 3 == 0 ? 0 : 3);
             int maxX = minX + 2;
             for (int y = minY; y <= maxY; y++)
             {
@@ -146,31 +146,32 @@ void makeSudoku(){
             //3.
             bool isSucces=false;
             int cnt = 0, val1 = 0, val2 = 0;
-            for (auto v : val)
+            for (int i = 1 ; i<10 ;i++)
             {
-                if (v > 0)
+                if (val[i] > 0)
                 {
                     cnt++;
-                    if(val1 == 0) val1 = v;
-                    else if(val2 == 0) val2 = v;
+                    if(val1 == 0) val1 = i;
+                    else if(val2 == 0) val2 = i;
                 }
             }
             if(blankCount == cnt){
                 isSucces =true;
-               if( map[order][row][col].size() ==1){
+                if( map[order][row][col].size() ==1){
                    map[order][row][col][0] = val1 + '0';
-               }else{
-                   if(map[order][row][col][0]=='-'){
-                        map[order][row][col][2] = val1 + '0';
-                   }
-                   else if(map[order][row][col][2]=='-'){
+                }else{
+                   if(map[order][row][col][0]=='-'&&map[order][row][col][2]!='-'){
                         map[order][row][col][0] = val1 + '0';
+                   }
+                   else if(map[order][row][col][0]!='-'&&map[order][row][col][2]=='-'){
+                        map[order][row][col][2] = val1 + '0';
                    }
                    else{
                         map[order][row][col][0] = val1 + '0';
                         map[order][row][col][2] = val2 + '0';
                    }
-               }
+                }
+                // cout<<"row :"<<row<<" col :"<<col<<" cnt:"<<cnt<<" ->"<<val1<<val2<<": "<<map[order][row][col]<<endl;
             }
 
             //4.
@@ -178,6 +179,7 @@ void makeSudoku(){
                 pos.push(make_pair(row,col));
             }
         }
+        cout<<"pos size: "<<pos.size()<<endl;
     }
 }
 
@@ -188,7 +190,7 @@ int main()
 
     for (int i = 0; i < gameCount; i++)
     {
-        int order;
+        int order; //게임 순서
         cin >> order;
         cin.ignore();
 
