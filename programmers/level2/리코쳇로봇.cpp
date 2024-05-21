5e785cc7d9cf0066e680e37ad8aeec6792c75dab
joh <dhgus110@naver.com>
2024-05-21 23:30:21 +0900

    bfs

--- programmers/level2/리코쳇로봇.cpp
+++ programmers/level2/리코쳇로봇.cpp
@@ -1,74 +1,86 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <iostream>
-#include <tuple>
 #include <queue>
 
 using namespace std;
+int isVisited[101][101];
+int dx[4] = {1, -1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
+int boardWidthSize, boardHightSize;
+pair<int,int> robotStartPos, goalPos;
 
-bool visited[101][101];
-int map[101][101]; //갈 수 없으면 0, 갈 수 있으면 2, 장애물 1, 목표위치 3
-int dx[4] = {0, 0, 1, -1}, dy[4] = {1, -1, 0, 0};
-int gx=0,gy=0;//목표위치
-int sx=0,sy=0; //스타트 위치
 
-int bfs(){
-    queue<pair<int,int>> q;
-    q.push({sx, sy});
-    visited[sx][sy]=true;
+void init(vector<string>const& board){
+    fill_n(&isVisited[0][0], 101 * 101, -1);
+    boardWidthSize = board[0].length();
+    boardHightSize = board.size();
 
-    while(!q.empty()){
-        int cx = q.front().first;
-        int cy = q.front().second;
-        q.pop();
-
-        for(int i = 0 ; i<4;i++){
-            int n = 1;
-            if (map[cx + dx[i] * n][cy + dy[i] * n]<2 ) //장애물을 만나거나 벽을 만나면
+    for (int i = 0; i < boardHightSize; i++){
+        for(int j = 0 ;j< boardWidthSize ; j++){
+            if(board[i][j]=='R') robotStartPos = make_pair(i,j);
+            if(board[i][j]=='G') goalPos = make_pair(i,j);
         }
     }
+}
 
+bool safeArea(int x, int y){
+    if (x >= 0 && y >= 0 && x < boardHightSize && y < boardWidthSize) return true;
+    return false;
+}
+
+
+pair<int,int> moveStraightGetPos(pair<int,int> robotPos , int dir, vector<string>const& board){
+    int x = robotPos.first;
+    int y = robotPos.second;
+    while(true){
+        int nextx = x + dx[dir];
+        int nexty = y + dy[dir];
+        if (safeArea(nextx, nexty)){
+            if(board[nextx][nexty]=='D'){
+                return make_pair(x,y);
+            }
+            else{
+                x= nextx; y =nexty;
+            }
+        }
+        else{
+            return make_pair(x,y);
+        }
+    }
+}
+
+//bfs 방식
+int run(vector<string>const& board){
+    queue<pair<int,int>> q ;
+    q.push(robotStartPos);
+    isVisited[robotStartPos.first][robotStartPos.second] +=1;
+
+    while(!q.empty()){
+        pair<int,int> pos = q.front();
+        q.pop();
+
+        for(int dir = 0 ; dir <4; dir++){
+            pair<int,int> nextPos = moveStraightGetPos(pos, dir ,board);
+            if (isVisited[nextPos.first][nextPos.second] == -1){
+                isVisited[nextPos.first][nextPos.second]
+                 = isVisited[pos.first][pos.second] + 1;
+
+                 q.push(nextPos);
+            }
+            if(board[nextPos.first][nextPos.second]=='G'){
+                return isVisited[nextPos.first][nextPos.second];
+            }
+        }
+
+    }
+    return -1;
 }
 
 int solution(vector<string> board) {
     int answer = 0;
-    int h = board.size()-1;
 
-    //init
-    for(auto bo : board){
-        int curx = 0;
-        for(auto b: bo){
-            if(b=='.')
-                map[h][curx]=2;
-            else if(b=='D')
-                map[h][curx]=1;
-            else if(b=='G'){
-                map[h][curx]=3;
-                gx=curx;
-                gy=h;
-            }else if(b=='R'){
-                map[h][curx]=1;
-                sx=curx;sy=h;
-            }
-            curx++;
-        }
-        h--;
-    }
-
-    //목표위치에서 상하좌우에 장애물이 없으면 실패
-    int cnt = 0;
-    for (int i = 0; i < 4; i++)
-    {
-        if (gx + dx[i] > 0 || gy + dy[i] > 0)
-        {
-            if (map[gx + dx[i]][gy + dy[i]] == 1)
-                cnt++;
-        }
-    }
-    if(cnt==4)return -1;
-
-
+    init(board);
+    answer = run(board);
 
     return answer;
 }
