da3a86ff821b5c9d9aa2906965ddae3887f3870a
joh <dhgus110@naver.com>
2024-05-25 18:24:33 +0900

    stack

--- programmers/level2/과제진행하기.cpp
+++ programmers/level2/과제진행하기.cpp
@@ -34,44 +34,58 @@ vector<string> run (){
         int curjobProcessTime = s.top().first[1];
         int curJobFinishTime = curJobStartTime + curjobProcessTime;
         string cur =s.top().second;
-        s.pop();
 
+        // 해야 할 작업이 남아 있을 때
         if (curOrder + 1 < convertMinPlans.size())
         {
             int nextJobStartTime = convertMinPlans[curOrder + 1].first[0];
 
+            // 다음 작업 시간에 맞춰서 딱 끝날 때
             if (curJobFinishTime == nextJobStartTime)
             {
                 result.push_back(cur);
+                s.pop();
                 s.push(convertMinPlans[++curOrder]);
             }
+            // 다음 작업 시간보다 더 해야할 때
             else if(curJobFinishTime > nextJobStartTime)
             {
                 curjobProcessTime -= (nextJobStartTime -curJobStartTime);
-                s.push(make_pair(vector{curJobStartTime, curjobProcessTime}, cur));
+                // s.push(make_pair(vector{curJobStartTime, curjobProcessTime}, cur));
+                s.top().first[1] = curjobProcessTime;
                 s.push(convertMinPlans[++curOrder]);
             }
+            // 다음 작업 시간보다 더 빨리 끝날 때
             else if (curJobFinishTime < nextJobStartTime)
             {
                 int gapTime = nextJobStartTime - curJobFinishTime ;
                 result.push_back(cur);
+                s.pop();
+
+                //gapTime만큼 스택에 대기하고 있던 과제 다시 하기
                 while(gapTime > 0 ){
+                    // 스택에 더 이상 아무것도 없을 때
                     if(s.empty()){
                         s.push(convertMinPlans[++curOrder]);
                         break;
                     }
-                    else if(gapTime > s.top().first[1]){
+                    
+                    //대기하던 과제가 gapTime보다 더 빨리 끝날 때 
+                    if(gapTime > s.top().first[1]){
                         result.push_back(s.top().second);
+                        gapTime -= s.top().first[1];
                         s.pop();
-                        if(!s.empty())
-                            gapTime -= s.top().first[1];
                     }
+
+                    //대기하던 과제가 gapTime만큼 딱 맞춰 끝날 때
                     else if ( gapTime == s.top().first[1]){
                         result.push_back(s.top().second);
                         s.pop();
                         s.push(convertMinPlans[++curOrder]);
                         gapTime = 0 ;
                     }
+
+                    //대기하던 과제가 gapTime에서 못 끝낼 때
                     else if(gapTime < s.top().first[1]){
                         s.top().first[1] -= gapTime;
                         s.push(convertMinPlans[++curOrder]);
@@ -83,6 +97,7 @@ vector<string> run (){
         }
         else
         {
+
             result.push_back(s.top().second);
             s.pop();
         }
@@ -95,6 +110,5 @@ vector<string> solution(vector<vector<string>> plans) {
     vector<string> answer;
     init(plans);
     SortAsc();
-
     return run();
 }
