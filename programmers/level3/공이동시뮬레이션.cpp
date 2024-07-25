acaffd38ebe86e44ee439494fa8cfcf0f08f49ad
joh <dhgus110@naver.com>
2024-07-25 19:38:41 +0900

    Update 공이동시뮬레이션.cpp

--- programmers/level3/공이동시뮬레이션.cpp
+++ programmers/level3/공이동시뮬레이션.cpp
@@ -17,8 +17,7 @@
 
 using namespace std;
 
-ll border[4] = {0, 0, 0, 0}; //왼쪽, 오른쪽, 아래쪽, 위쪽 마지막 면
-ll movex =0, movey =0; // 총 이동 거리
+vector<vector<ll>> ballPosition(4,vector<ll>(2,0));
 
 void print(vector<int>const& v){
     for(auto vv : v){
@@ -26,95 +25,172 @@ void print(vector<int>const& v){
     }cout<<endl;
 }
 
-//todo  dx가 점과 진행방향의 벽 사이의 거리보다 크다면 아무의미도 없으니 어떻게 처리할까....
-pair<ll,ll> run(ll x, ll y, vector<vector<int>>const& queries){
-    for(auto q: queries){
-        //왼쪽으로 이동
-        if(q[0] == 0){
-
-            //이동한 공이 경계면에서 벗어 났을 때
-            if( x - q[1] < border[q[0]]){
-                border[q[0]] = border[q[0]] + q[1] - (x - border[q[0]]);
-                x = border[q[0]];
-            }
-
-            //이공한 공이 경계면에서 벗어 나지 않았을 때
-            else x -= q[1];
-
-            //이동한 공이 반대벽과 만났을 시
-            if(x > border[1]) x = border[q[0]] = border[1];
-            movex -= q[1];
+void run(int n , int m, vector<vector<int>>const& queries){
+    for(auto q :queries){
+        switch(q[0])
+        {
+            case 0:
+                ballPosition[0][0] -=  q[1];// 왼쪽
+                ballPosition[1][0] -=  q[1];// 오른쪽
+                break;
+            case 1:
+                ballPosition[0][0] +=  q[1];// 왼쪽
+                ballPosition[1][0] +=  q[1];// 오른쪽
+                break;
+            case 2:
+                ballPosition[2][0] -=  q[1];// 위쪽
+                ballPosition[3][0] -=  q[1];// 아래쪽
+                break;
+            case 3:
+                ballPosition[2][0] +=  q[1];// 위쪽
+                ballPosition[3][0] +=  q[1];// 아래쪽
+                break;
         }
-        //오른쪽으로 이동
-        else if (q[0] == 1){
-            if( x + q[1] > border[q[0]]){
-                border[q[0]] = border[q[0]] - (q[1] - (border[q[0]] - x));
-                x = border[q[0]];
-            }
-            else x += q[1];
-            if(x < border[0]) x = border[q[0]] = border[0];
-            movex += q[1];
+        // 격자 map 를 나가는 경우 나간 만큼 접으면서 곂침 처리
+        if(ballPosition[0][0] < 0)
+        {
+            ballPosition[0][1] += ( - ballPosition[0][0]);
+            ballPosition[0][0] = 0;
         }
-        //아래로 이동
-        else if (q[0] == 2){
-            if (y - q[1] < border[q[0]]){
-                border[q[0]] = border[q[0]] + q[1] - (y - border[q[0]]);
-                y = border[q[0]];
-            }
-            else y -= q[1];
-            if(y > border[3]) y = border[q[0]] = border[3];
-            movey -= q[1];
+        if(ballPosition[0][0] >= m)
+        {
+            ballPosition[0][1] = m;
+            ballPosition[0][0] = m-1;
         }
-        //위로 이동
-        else if (q[0] == 3){
-            if( y + q[1] > border[q[0]]){
-                border[q[0]] = border[q[0]] - (q[1] - (border[q[0]] - y));
-                y = border[q[0]];
-            }
-            else y += q[1];
-            if(y < border[2]) y = border[q[0]] = border[2];
-            movey += q[1];
+        if(ballPosition[1][0] >= m)
+        {
+            ballPosition[1][1] += (ballPosition[1][0] - m + 1);
+            ballPosition[1][0] = m-1;
+        }
+        if(ballPosition[1][0] < 0)
+        {
+            ballPosition[0][1] = m;
+            ballPosition[1][0] = 0;
+        }
+        if(ballPosition[2][0] < 0)
+        {
+            ballPosition[2][1] += ( - ballPosition[2][0]);
+            ballPosition[2][0] = 0;
+        }
+        if(ballPosition[2][0] >= n)
+        {
+            ballPosition[2][1] = n;
+            ballPosition[2][0] = n-1;
+        }
+        if(ballPosition[3][0] >= n)
+        {
+            ballPosition[3][1] += (ballPosition[3][0] - n + 1);
+            ballPosition[3][0] = n-1;
+        }
+        if(ballPosition[3][0] < 0)
+        {
+            ballPosition[3][1] = n;
+            ballPosition[3][0]  = 0;
         }
     }
 
-    return {x,y};
+    // 격자 맵 크기보다 큰 경우 격자 크기로 조절
+    if(ballPosition[0][1] > m) ballPosition[0][1] = m;
+    if(ballPosition[1][1] > m) ballPosition[1][1] = m;
+    if(ballPosition[2][1] > n) ballPosition[2][1] = n;
+    if(ballPosition[3][1] > n) ballPosition[3][1] = n;
 }
 
-ll operation(pair<ll, ll> pos, int n, int m){
-    ll foldx = 1, foldy = 1;
-    ll curx = pos.first;
-    ll cury = pos.second;
+ll operation(int n, int m, int x, int y){
+    ll answer = -1;
+    
+    // 접힌 배열의 l,r,u,d 좌표
+    ll  sy = ballPosition[0][0];
+    ll  ey = ballPosition[1][0];
+    ll  sx = ballPosition[2][0];
+    ll  ex = ballPosition[3][0];
 
-    if (curx <= border[0] && curx < border[1]) foldx = border[0] + 1;
-    else if (curx > border[0] && curx >= border[1]) foldx = m - border[1] + 1;
-    else if (border[0] == border[1]) foldx = m;
+    // 접힌 배열이 x,y 에 없는 경우 0 리턴
+    if( (sx <= x && sy <= y) && 
+        (ex >= x && ey >= y) )
+    {
+        answer = m*n;
+    } else
+    {
+        return 0;
+    }
 
-    if (cury <= border[2] && cury < border[3]) foldy = border[2] + 1;
-    else if (cury > border[2] && cury >= border[3]) foldy = n - border[3] + 1;
-    else if (border[2] == border[3]) foldy = n;
- 
+    // 접힌 곂침 수 추출
+    ll  sym = ballPosition[0][1];
+    ll  eym = ballPosition[1][1];
+    ll  sxm = ballPosition[2][1];
+    ll  exm = ballPosition[3][1];
 
-    for(int i = 0 ; i <4; i++) cout<<border[i]<<" ";
-    cout<<endl<<endl;
-    cout<<"현재 점의 위치 : "<<curx<<" "<<cury<<endl;
-    cout<<"접힘 수 : "<<foldx<<" "<<foldy<<endl;
-    cout<<"총 이동 거리 : "<< movex<<" "<<movey<<endl;
+    // 곂쳐진 x 값이 1개이면 곂침 수를 n 으로 통일
+    if(sx == ex) 
+    {
+        sxm = n;
+        exm = n;        
+    }
+    // 곂쳐진 y 값이 1개이면 곂침 수를 m 으로 통일
+    if(sy == ey) 
+    {
+        sym = m;
+        eym = m;
+    }
 
-    return foldx * foldy;
-}
-
-long long solution(int n, int m, int x, int y, vector<vector<int>> queries) {
-    long long answer = -1;
-    //init
-    border[1] = m - 1;
-    border[3] = n - 1;
-    reverse(queries.begin(),queries.end());
-
-    //쿼리를 거꾸로 실행 후 점의 위치와 경계면들의 접힘의 정도
-    auto position = run(y, x, queries);
-
-    //시작점으로 있을 수 있는 위치들을 계산
-    answer = operation(position, n ,m);
+    if(x == sx && y == sy)
+    {
+        // x,y 가 왼쪽 위 모서리인 경우
+        answer = sxm * sym;
+    } else if(x == sx && y == ey)
+    {
+        // x,y 가 오른쪽 위 모서리인 경우
+        answer = sxm * eym;
+    } else if(x == ex && y == sy)
+    {
+        // x,y 가 왼쪽 아래 모서리인 경우
+        answer = exm * sym;
+    } else if(x == ex && y == ey)
+    {
+        // x,y 가 오른쪽 아래 모서리인 경우
+        answer = exm * eym;
+    } else if(x == sx && (y > sy && y < ey) )
+    {
+        // x,y 가 위쪽 변인 경우
+        answer = sxm;
+    } else if(x == ex && (y > sy && y < ey) )
+    {
+        // x,y 가 아래쪽 변인 경우
+        answer = exm;
+    } else if(y == sy && (x > sx && x < ex) )
+    {
+        // x,y 가 왼쪽 변인 경우
+        answer = sym;
+    } else if(y == ey && (x > sx && x < ex) )
+    {
+        // x,y 가 오른쪽 변인 경우
+        answer = eym;
+    } else
+    {
+        // x,y 가 접히지 않은 위치인 경우
+        answer = 1;
+    }
 
     return answer;
+}
+
+
+long long solution(int n, int m, int x, int y, vector<vector<int>> queries) {
+    //init
+    ballPosition[0][0] = 0;
+    ballPosition[1][0] = m - 1;
+    ballPosition[2][0] = 0;
+    ballPosition[3][0] = n - 1;
+    for(int i=0;i<4;i++)
+    {
+        ballPosition[i][1] = 1;
+    }
+    // reverse(queries.begin(),queries.end());
+
+    //점의 위치와 경계면들의 접힘의 정도
+    // auto position = run(y, x, queries);
+    run(n, m, queries);
+
+    return operation(n,m,x,y);
 }
