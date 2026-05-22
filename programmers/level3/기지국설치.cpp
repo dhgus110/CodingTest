d67683864e027ad845040627efc4006367d8e2b2
dhgus110 <dhgus110@naver.com>
2026-05-22 23:03:13 +0900

    코테

--- programmers/level3/기지국설치.cpp
+++ programmers/level3/기지국설치.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int solution(int n, vector<int> stations, int w)
 {
-    int answer = 0;
-    
+    int answer = 0, wave = 0 ,index = 0;
     int range = 2 * w + 1;
-    int index = 0;
-    int wave = 0;
-    wave =  stations[index] - w <= 1 ? stations[++index] + w : wave; 
-   
+    
+    if(!stations.empty() && stations[index] - w <= 1)
+        wave = stations[index++] + w;
+
+
     while(n > wave)
     {
         //cout<< " 시작 " << wave<<endl;
@@ -20,11 +20,13 @@ int solution(int n, vector<int> stations, int w)
         while( wave >= endStation && index < stations.size())
         {
             ++index;
+            if( index >= stations.size()) break;
+
             beginStation = stations[index] - w;
             endStation = stations[index] + w;
         }
 
-        while(beginStation <= wave && wave <= endStation && index < stations.size())
+        while(beginStation - 1 <= wave && wave <= endStation && index < stations.size())
         {
             wave = endStation;
             //cout<< " 기지국 " << wave<<endl;
@@ -41,7 +43,7 @@ int solution(int n, vector<int> stations, int w)
             int remain = n - wave ;
             int dummy = (remain % range) > 0 ? 1 : 0;
             answer += (remain/range) + dummy;
-           // cout<< " 나머지 " << wave + remain <<endl;
+            //cout<< " 나머지 " << wave + remain <<endl;
 
             break;
         }
@@ -51,10 +53,9 @@ int solution(int n, vector<int> stations, int w)
             wave += range;
             ++answer;   
         }
-        //cout<< " 한칸 " << wave<<endl;
+       // cout<< " 한칸 " << wave<<endl;
 
     }
 
     return answer;
 }
-
