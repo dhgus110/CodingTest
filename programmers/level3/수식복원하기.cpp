3785f83626fa00f7cd9e144f9ead93caf33152c8
dhgus110 <dhgus110@naver.com>
2026-05-29 20:40:47 +0900

    진법 변환

--- programmers/level3/수식복원하기.cpp
+++ programmers/level3/수식복원하기.cpp
@@ -1,112 +1,98 @@
 #include <string>
 #include <vector>
-#include <cctype>
+#include <iostream>
 
 using namespace std;
-//n진수 -> 10진수 
-int tenConversion(int base, int n)
+
+vector<vector<int>> question;
+vector<vector<int>> modify;
+vector<bool> isBase(10,false);
+int maxBase = 0;
+
+void init(const vector<string>& expresstions)
+{
+    for(const string& ex : expresstions)
+    {
+        vector<int> tempV;
+        string tempStr = "";
+        bool isQuestion = false;
+        for(char e : ex)
+        {
+            int num = e -'0';
+            if(e==' ' && !tempStr.empty()){
+                tempV.emplace_back(stoi(tempStr));
+                tempStr = "";
+            }
+            else if( e == '+')tempV.emplace_back(1);
+            else if( e == '-') tempV.emplace_back(-1);
+            else if(0 <= num && num <=9 ){
+                tempStr += e;
+                maxBase = max(maxBase, num);
+            }
+            else if(e == 'X') isQuestion = true;
+        }
+        if(isQuestion)
+            question.emplace_back(tempV);
+        else{
+            tempV.emplace_back(stoi(tempStr));
+            modify.emplace_back(tempV);
+        }
+    }
+
+    ++maxBase;
+}
+
+// n -> 10
+int tenConversion(int nBase, int num)
 {
     int res = 0;
-    int tempBase = 1;
-    while(n > 0)
+    int multi = 1;
+
+    while( num > 0)
     {
-        res += (n % 10) * tempBase;
-        tempBase *= base;
-        n /= 10;
-    }
-    return res;
-}
-// 10진수 -> n진수
-int numConversion(int base, int n)
-{
-    int res = 0 ;
-    int tempBase = 1;
-    while(n > 0)
-    {
-        res += (n % base) * tempBase;
-        tempBase *= 10;
-        n /= base;
+        res += (num % 10) * multi;
+        multi *= nBase;
+        num /= 10;
     }
     return res;
 }
 
-int checkNumeral(int base, const vector<vector<int>>& expressions)
+//10 -> n
+int nConvertion(int nBase, int num)
 {
-    bool check = true;
-    for(const auto& ex : expressions)
+    int res = 0;
+    int multi = 1;
+    while( num > 0)
     {
-        int a = tenConversion(base,ex[0]);
-        int b = tenConversion(base,ex[2]);
-        int c = tenConversion(base,ex[3]);
-        if(ex[1] == 1 && a+b != c) 
+        res +=(num % nBase) * multi;
+        multi *= 10;
+        num /= nBase;
+    }
+    return res;
+}
+
+void checkBase(int base, int num)
+{
+
+}
+
+void run()
+{
+    for(const auto& mo : modify)
+    {
+        for(int i = 2 ; i <10 ; ++i)
         {
-            check =false;
-            break;
-        }
-        else if( a-b != c) 
-        {
-            check = false;
-            break;
+            if(isBase[i]) continue;
+            
         }
     }
-    if(check) return base;
-    else checkNumeral(base+1,expressions);
 }
+
 
 vector<string> solution(vector<string> expressions) {
     vector<string> answer;
-    
-    vector<vector<int>> question;
-    vector<vector<int>> modify;
-    int maxValue = 0 ;
-    
-    for(const string& e : expressions)
-    {
-        string tempStr="";
-        vector<int> tempV;
-            
-        for(char c : e)
-        {
-            if(c== ' ' && !tempStr.empty()) {
-                tempV.emplace_back(stoi(tempStr));
-                tempStr ="";
-            }
-            else if(c == '+') tempV.emplace_back(1);
-            else if(c == '-') tempV.emplace_back(-1);
-            else if(c == '=' || c==' ') continue;
-            else 
-            {
-                tempStr += c;
-                maxValue = max( maxValue, c - '0');
-            }
-        }
-        if(tempStr == "X")
-        {
-            question.push_back(tempV);
-        }
-        else
-        {
-            tempV.emplace_back(stoi(tempStr));
-            modify.push_back(tempV);
-        }
-    }
-    
-    int baseNumeral = checkNumeral(maxValue + 1 , modify);
-    
-    for(const auto& q : question)
-    {
-        string temp = to_string(q[0])+" ";
-        if(q[1] == 1) temp +="+ "+to_string(q[2])+" = ";
-        else temp +="- "+to_string(q[2])+" = ";
-        
-        int a = tenConversion(baseNumeral, q[0]);
-        int b = tenConversion(baseNumeral, q[2]);
-        int c = q[1] == 1 ? a + b : a-b;
-        c = numConversion(baseNumeral, c);
-        temp+=(to_string(c));
-        answer.emplace_back(temp);
-    }
-    
-    
+    init(expressions);
+
+
     return answer;
 }
